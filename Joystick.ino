0f564b48d9b44a6030198c54eef08c9a7dd44fb8
AriYB <artiger61@gmail.com>
2017-01-30 19:51:19 -0500

    Added first protothread skeleton

--- Joystick.ino
+++ Joystick.ino
@@ -1,6 +1,21 @@
+#include <lc-addrlabels.h>
+#include <lc-switch.h>
+#include <lc.h>
+#include <pt-sem.h>
+#include <pt.h>
+
 void setup() {
   // put your setup code here, to run once:
 
+}
+
+//This is a skeleton of a protothread. 
+static int protothread1(struct pt *pt) { 
+  PT_BEGIN(pt); 
+  while(1) {           
+    PT_WAIT_UNTIL(pt, true);  
+    } 
+    PT_END(pt);
 }
 
 void loop() {
