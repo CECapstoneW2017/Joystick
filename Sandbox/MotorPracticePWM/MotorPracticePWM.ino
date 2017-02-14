7a180ec4a110ec6d30098134d1ddcb6f4164fc79
AriYB <artiger61@gmail.com>
2017-02-14 16:12:35 -0500

    New H-bridge
    New H-Bridge handles the signals a little differently.

--- Sandbox/MotorPracticePWM/MotorPracticePWM.ino
+++ Sandbox/MotorPracticePWM/MotorPracticePWM.ino
@@ -1,3 +1,8 @@
+//Motor 1
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+//Motor 2
 
 int motorPin = 3;
 int delayTime = 200;
@@ -5,16 +10,22 @@ int delayHigh = 10;
 void setup() {
   // put your setup code here, to run once:
   pinMode(motorPin, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  /*
   digitalWrite(motorPin, HIGH);
   delayMicroseconds(delayHigh);
   digitalWrite(motorPin, LOW);
   delayMicroseconds(delayTime);
-  
-  //analogWrite(motorPin, 30);
+  */
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+
+  analogWrite(motorPin, 200);
+
 }
