49deb988a9bd47e9ce93623a3c5ab6625a85eb76
AriYB <artiger61@gmail.com>
2017-02-14 21:27:27 -0500

    Updated Calculation
    Uses a Pot to change the motor duty cycle and direction. Proof of
    concept of using the H-Bridge.

--- Sandbox/MotorPracticePWM/MotorPracticePWM.ino
+++ Sandbox/MotorPracticePWM/MotorPracticePWM.ino
@@ -1,31 +1,44 @@
+#define MOTOR_PIN_1 5
+#define MOTOR_PIN_2 6
+#define POT_1 A0
+
 //Motor 1
 int enA = 10;
 int in1 = 9;
 int in2 = 8;
 //Motor 2
 
-int motorPin = 3;
-int delayTime = 200;
-int delayHigh = 10;
+double motorADirection1;
+
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motorPin, OUTPUT);
+  pinMode(MOTOR_PIN_1, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
+  Serial.begin(9600);
   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  /*
-  digitalWrite(motorPin, HIGH);
-  delayMicroseconds(delayHigh);
-  digitalWrite(motorPin, LOW);
-  delayMicroseconds(delayTime);
-  */
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
+  //Motor Direction determined by potentiometer
+  motorADirection1 = ((analogRead(POT_1) - 512) / 2);
 
-  analogWrite(motorPin, 200);
+  if(motorADirection1 > 0)
+  {
+    //Motor A Clockwise
+    digitalWrite(in2, LOW);
+    digitalWrite(in1, HIGH);
+    
+  }
+  else {
+    //Motor A Counter-clockwise
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+  }
+  analogWrite(MOTOR_PIN_1, abs(motorADirection1));
+ 
+  Serial.println(motorADirection1);
+  delay(10);
 
 }
