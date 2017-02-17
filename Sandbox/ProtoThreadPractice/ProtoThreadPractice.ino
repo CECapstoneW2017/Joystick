ad79fec80d1282f40c9c993ad72f8db648f6f8ff
AriYB <artiger61@gmail.com>
2017-02-17 00:43:02 -0500

    Finished Structure of Protothread prototype

--- Sandbox/ProtoThreadPractice/ProtoThreadPractice.ino
+++ Sandbox/ProtoThreadPractice/ProtoThreadPractice.ino
@@ -1,13 +1,14 @@
 //This is a test program for learning protothreading
 #include <pt.h>   // include protothread library
 
+#define BAUD_RATE 115200 //Faster than 9600
 #define LEDPIN 13  // LEDPIN is a constant 
 #define POT1PIN 11
 #define POT2PIN 12
 #define MOTOR_PIN_1 5 //Pin supports PWM
 #define MOTOR_PIN_2 6 //Pin that supports PWM
-#define ANGLE_FACTOR_1 1 // Factor to calculate angle from pot signal
-#define ANGLE_FACTOR_2 1 // Factor to calculate angle from pot signal
+#define ANGLE_FACTOR_1 0.25 // Factor to calculate angle from pot signal
+#define ANGLE_FACTOR_2 0.25 // Factor to calculate angle from pot signal
 
 int pot1 = 0;
 int pot2 = 0;
@@ -18,9 +19,9 @@ int motorTorque2 = 0;
 
 String inputString = "";
 boolean stringComplete = false;
-char inChar = (char)Serial
 
-static struct pt pt1, pt2, pt3; // each protothread needs one of these
+
+static struct pt pt1, pt2, pt3, pt4; // each protothread needs one of these
 /*
  * Priorities of tasks:
  * First task: Read the serial input from CHAI3D
@@ -38,7 +39,7 @@ void setup() {
   
   PT_INIT(&pt1);  // initialise the two
   PT_INIT(&pt2);  // protothread variables
-  
+  Serial.begin(BAUD_RATE);
 }
 
 void toggleLED() {
@@ -56,35 +57,26 @@ static int protothread1(struct pt *pt, int interval) {
     *  argument "millis() - timestamp > interval" is re-evaluated
     *  and if false the function exits after that. */
     PT_WAIT_UNTIL(pt, millis() - timestamp > interval );
+    timestamp = millis(); // take a new timestamp
+    
     pot1 = analogRead(POT1PIN); //Top potentiometer
     angle1 = ANGLE_FACTOR_1 * (pot1 - 512);
     pot2 = analogRead(POT2PIN); //Top potentiometer
     angle2 = ANGLE_FACTOR_2 * (pot2 - 512);
-    Serial.println("Angle 1: " + angle1);
-    Serial.println("Angle 2: " + angle2);
+    
   }
   PT_END(pt);
 }
 
-/* Receive the String from the driver for motor*/
-static int protothread3(struct pt *pt, int interval) {
-  static unsigned long timestamp = 0;
-  PT_BEGIN(pt);
-  while(1) {
-    PT_WAIT_UNTIL(pt, Serial.available() );
-    
-    analogWrite(MOTOR_PIN_1, motorTorque1);
-    analogWrite(MOTOR_PIN_2, motorTorque2);
-  }
-  PT_END(pt);
-}
+
 
 /* Generate the motor Torque*/
 static int protothread2(struct pt *pt, int interval) {
   static unsigned long timestamp = 0;
   PT_BEGIN(pt);
   while(1) {
-    PT_WAIT_UNTIL(pt, interval );
+    PT_WAIT_UNTIL(pt, millis() - timestamp > interval  );
+    timestamp = millis(); // Set new timestamp
     /*
     digitalWrite(motorPin, HIGH);
     delayMicroseconds(delayHigh);
@@ -97,8 +89,34 @@ static int protothread2(struct pt *pt, int interval) {
   PT_END(pt);
 }
 
+/* Receive the String from the driver for motor*/
+static int protothread3(struct pt *pt, int interval) {
+  static unsigned long timestamp = 0;
+  PT_BEGIN(pt);
+  while(1) {
+    PT_WAIT_UNTIL(pt, Serial.available() ); // What should the condition be?
+    //Read from the serial input buffer
+    //Process Strings to get each motorTorquePercent
+  }
+  PT_END(pt);
+}
+
+/* Send the Angle position over serial*/
+static int protothread4(struct pt *pt, int interval) {
+  static unsigned long timestamp = 0;
+  PT_BEGIN(pt);
+  while(1) {
+    PT_WAIT_UNTIL(pt, Serial.available() ); // What should the condition be?
+    //Read from the serial input buffer
+    Serial.println(angle1 + ' ' + angle2);
+  }
+  PT_END(pt);
+}
+
 void loop() {
   //Left these two threads like this for now.
-  protothread1(&pt1, 10); // schedule the two protothreads
-  protothread2(&pt2, 1000); // by calling them infinitely
+  protothread1(&pt1, 10);     //Angle Thread
+  protothread2(&pt2, 1000);   //Motor Thread
+  protothread3(&pt3, 10);     //Serial Input Thread
+  protothread4(&pt4, 1000);
 }
